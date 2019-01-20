6d7f0e8ccae68fc84e379a7d2cbe5992d5db9ced
Akanksha Choudhary <akanksha.ch29@gmail.com>
2019-01-20 17:22:00 +0530

    commenting error plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-catch-links',
-    `gatsby-transformer-sharp`,
+    // `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: 'gatsby-source-filesystem',
