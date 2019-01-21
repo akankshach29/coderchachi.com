57d1d9bd62d1ff9e1857cc6d0141e6379240e080
Akanksha Choudhary <akanksha.ch29@gmail.com>
2019-01-21 18:49:47 +0530

    google analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,21 +38,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
+      // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-73324242-2",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "GTM-59MLVW2",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
       },
     },
     'gatsby-plugin-offline',
