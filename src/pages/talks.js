be37590ca7b8f35c5ceed5d8451749756e35a2a0
Akanksha Choudhary <akanksha.ch29@gmail.com>
2019-03-04 20:59:05 +0530

    talks updated

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -26,11 +26,21 @@ const TalkPage = () => (
       <p style={{ margin: '5px 0px 10px', fontSize: '0.9em', fontWeight: 'bold'}}>Speaking at: <a style={{ color: '#77bfd6', textDecoration: 'none' }} href="https://www.meetup.com/ReactJS-Bangalore/events/257950984/">ReactJS Bangalore</a></p>
       <p>
          Gatsby is a blazing fast modern site generator for React. 
-         Its offers great advantages like code splitting, server-side rendering,
+         It offers great advantages like code splitting, server-side rendering,
          intelligent image loading,  data prefetching, etc.
          And hosting the site with Netlify is an added bonus. My talk covers a
          basic website building with Gatsby and hosting it with Netlify and some
          insights on Netlify-CMS.
+      </p>
+      <br />
+      <h2 className="blogSquare__title">The Cost of GraphQL</h2>
+      <p style={{ margin: '5px 0px 0px', fontSize: '0.8em', fontWeight: 'bold'}}>April 12th & 13th, 2019</p>
+      <p style={{ margin: '5px 0px 10px', fontSize: '0.9em', fontWeight: 'bold'}}>Speaking at: <a style={{ color: '#77bfd6', textDecoration: 'none' }} href="https://www.graphql-asia.org/">GraphQL Asia</a></p>
+      <p>
+      REST has been around for a while now and implementing GraphQL into existing
+      apps take time. Cost of GraphQL is basically the time it is taking for 
+      startups to adapt GraphQL. I will be sharing the story of adapting GraphQL
+      in my company.
       </p>
       
     </section>
